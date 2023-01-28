f40a000370ea57097c47569c0f557ec200ab979e
kristinavan <chr1st1nko007@gmail.com>
2023-01-28 16:42:20 -0500

    upded tile calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import {useState} from "react";
+import { useState } from 'react';
 
 function App() {
+    const [areaWidth, setAreaWidth] = useState(0); // ft
+    const [areaHeight, setAreaHeight] = useState(0); // ft
+    const [areaAddition, setAreaAddition] = useState(10); // 10% addition
 
-    const [areaWidth, setAreaWidth] = useState(0);
-    const [areaHeight, setAreaHeight] = useState(0);
-    const [areaAddition, setAreaAddition] = useState(10);
+    const [area, setArea] = useState(0); // ft^2
 
-    const [area, setArea] = useState(0);
+    const [tilesWidth, setTilesWidth] = useState(0); // in
+    const [tilesHeight, setTilesHeight] = useState(0); // in
+    const [sellOption, setSellOption] = useState(null); // box, piece
+    const [tilesInBox, setTilesInBox] = useState(0); // pieces in box
 
-    const [tilesWidth, setTilesWidth] = useState(0);
-    const [tilesHeight, setTilesHeight] = useState(0);
-    const [sellOption, setSellOption] = useState('');
-    const [tilesInBox, setTilesInBox] = useState(0);
-    const [boxQuantity, setBoxQuantity] = useState(0);
-    const [tilesPrice, setTilesPrice] = useState(0);
+    const [boxQuantity, setBoxQuantity] = useState(0); // boxes
+    const [tilesPrice, setTilesPrice] = useState(0); // $ price per box or piece
 
-    const [tilesAmount, setTilesAmount] = useState(0);
+    const [tilesAmount, setTilesAmount] = useState(0); // pcs
 
-    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalPrice, setTotalPrice] = useState(0); // $
 
     const calculate = (obj) => {
         const data = {
@@ -28,12 +28,12 @@ function App() {
             tilesHeight,
             sellOption,
             tilesInBox,
-            tilesPrice
-        }
+            tilesPrice,
+        };
 
-        const merge = {...data, ...obj}
+        const merge = { ...data, ...obj };
 
-        const totalArea = Math.ceil(merge.areaWidth * merge.areaHeight * (1 + merge.areaAddition / 100));
+        const totalArea = Math.ceil(merge.areaWidth * 12 * (merge.areaHeight * 12) * (1 + merge.areaAddition / 100));
         setArea(totalArea);
 
         const tilesArea = merge.tilesWidth * merge.tilesHeight;
@@ -41,96 +41,88 @@ function App() {
         setTilesAmount(tilesAmount);
 
         let boxQuantity = 0;
-        if (merge.sellOption === "box") {
+        if (merge.sellOption === 'box') {
             boxQuantity = Math.ceil(tilesAmount / merge.tilesInBox);
             setBoxQuantity(boxQuantity);
         }
 
-        if (merge.sellOption === "piece") {
+        if (merge.sellOption === 'piece') {
             setBoxQuantity(0);
         }
 
-        const totalPrice = merge.sellOption === "box" ? merge.tilesPrice * boxQuantity : merge.tilesPrice * tilesAmount;
+        const totalPrice = merge.sellOption === 'box' ? merge.tilesPrice * boxQuantity : merge.tilesPrice * tilesAmount;
         setTotalPrice(totalPrice);
-    }
-
+    };
 
     const areaWidthChange = (e) => {
         setAreaWidth(e.target.value);
-        calculate({areaWidth: +e.target.value});
-    }
+        calculate({ areaWidth: +e.target.value });
+    };
 
     const areaHeightChange = (e) => {
         setAreaHeight(e.target.value);
-        calculate({areaHeight: +e.target.value});
-    }
+        calculate({ areaHeight: +e.target.value });
+    };
 
     const areaAdditionChange = (e) => {
         setAreaAddition(e.target.value);
-        calculate({areaAddition: +e.target.value});
-    }
+        calculate({ areaAddition: +e.target.value });
+    };
 
     const tilesWidthChange = (e) => {
-        setTilesWidth(e.target.target);
-        calculate({tilesWidth: +e.target.value});
-    }
+        setTilesWidth(e.target.value);
+        calculate({ tilesWidth: +e.target.value });
+    };
 
-    const tileHeightChange = (e) => {
+    const tilesHeightChange = (e) => {
         setTilesHeight(e.target.value);
-        calculate({tilesHeight: +e.target.value});
-    }
+        calculate({ tilesHeight: +e.target.value });
+    };
 
     const sellOptionChange = (e) => {
         setSellOption(e.target.value);
-        calculate({sellOption: e.target.value});
-    }
+        calculate({ sellOption: e.target.value });
+    };
 
     const tilesInBoxChange = (e) => {
         setTilesInBox(e.target.value);
-        calculate({tilesInBox: +e.target.value});
-    }
+        calculate({ tilesInBox: +e.target.value });
+    };
 
     const tilesPriceChange = (e) => {
         setTilesPrice(e.target.value);
-        calculate({tilesPrice: +e.target.value});
-    }
-
+        calculate({ tilesPrice: +e.target.value });
+    };
 
     return (
         <div className="container">
-            <h1 className="text-center">Tile calculator</h1>
+            <h1>Tile Shop</h1>
+
             <div className="row">
                 <div className="col-md-4">
                     <label>Area Width, ft</label>
-                    <input type="number" className="form-control" value={areaWidth}
-                           onChange={areaWidthChange}/>
+                    <input type="number" className="form-control" value={areaWidth} onChange={areaWidthChange} />
                 </div>
-
                 <div className="col-md-4">
                     <label>Area Height, ft</label>
-                    <input type="number" className="form-control" value={areaHeight}
-                           onChange={areaHeightChange}/>
+                    <input type="number" className="form-control" value={areaHeight} onChange={areaHeightChange} />
                 </div>
-
                 <div className="col-md-4">
-                    <label>Area Additional %</label>
-                    <input type="number" className="form-control" value={areaAddition}
-                           onChange={areaAdditionChange}/>
+                    <label>Area Addition Percent</label>
+                    <input type="number" className="form-control" value={areaAddition} onChange={areaAdditionChange} />
                 </div>
             </div>
 
-            <div className="row">
-                <div className="col-md-4">
-                    <label>Tile Width, in</label>
-                    <input type="number" className="form-control" value={tilesWidth}
-                           onChange={tilesWidthChange}/>
+            <div className="row mt-5">
+                <div className="col-md-3">
+                    <label>Tiles Width, in</label>
+                    <input type="number" className="form-control" value={tilesWidth} onChange={tilesWidthChange} />
                 </div>
-                <div className="col-md-4">
-                    <label>Tile Height, in</label>
-                    <input type="number" className="form-control" value={tilesHeight}
-                           onChange={tileHeightChange}/>
+                <div className="col-md-3">
+                    <label>Tiles Height, in</label>
+                    <input type="number" className="form-control" value={tilesHeight} onChange={tilesHeightChange} />
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-3">
                     <label>Sell Option</label>
                     <select className="form-select" aria-label="Sell Option" onChange={sellOptionChange}>
                         <option value="">Select</option>
@@ -138,28 +130,41 @@ function App() {
                         <option value="piece">Piece</option>
                     </select>
                 </div>
+
+                <div className="col-md-3">
+                    <label>Price per {sellOption === 'box' ? 'box' : 'piece'}</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        placeholder="Price"
+                        value={tilesPrice}
+                        onChange={tilesPriceChange}
+                    />
+                </div>
             </div>
 
-            {sellOption === "box" && (
-                <div>
-                    <label>Tiles in Box</label>
-                    <input type="number" className="form-control" placeholder="Box Quantity" value={tilesInBox}
-                           onChange={tilesInBoxChange}/>
+            {sellOption === 'box' && (
+                <div className="row mt-2">
+                    <div className="col-md-3">
+                        <label>Tiles in Box</label>
+                        <input
+                            type="number"
+                            className="form-control"
+                            placeholder="Box Quantity"
+                            value={tilesInBox}
+                            onChange={tilesInBoxChange}
+                        />
+                    </div>
                 </div>
             )}
-            Price per {sellOption}:
-            <input type="number" className="form-control" placeholder="Price" value={tilesPrice}
-                   onChange={e => tilesPriceChange(e)}/>
 
             <div className="mt-5">
-                <div> Area: {area} sq in</div>
-                <div> Tiles Amount: {tilesAmount}</div>
+                <div>Area: {area} sq in</div>
+                <div>Tiles Amount: {tilesAmount}</div>
 
-                {sellOption === "box" && <div>Box Quantity: {boxQuantity}</div>}
-                <div>Total Price: ${totalPrice}</div>
+                {sellOption === 'box' && <div>Box Quantity: {boxQuantity}</div>}
+                <div>Total Price: {totalPrice}</div>
             </div>
-
-
         </div>
     );
 }
